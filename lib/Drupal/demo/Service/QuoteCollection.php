feaa9732061e7f89d5ed870309dea41eadaadd6d
matt foster <mattfoster01@gmail.com>
2013-08-28 13:35:02 -0400

    Added new code for services article

--- lib/Drupal/demo/Service/QuoteCollection.php
+++ lib/Drupal/demo/Service/QuoteCollection.php
@@ -5,58 +5,69 @@ namespace Drupal\demo\Service;
 
 class QuoteCollection
 {
-  
-  protected $author;
-   
-  public function __construct($quotes)
-  {
-    $this->quotes = $quotes;
-  }
-  
-  /**
-   * Set the author of the collection. This enforces that the entire
-   * collection must be "owned" by a single author.
-   * 
-   * @access public
-   * @param mixed $author
-   * @return void
-   */
-  public function setAuthor($author)
-  {
-    $this->author = $author;  
-  }
-  
-  /**
-   * Returns the author of the quote collection.
-   * 
-   * @access public
-   * @return string
-   */
-  public function getAuthor()
-  {
-    return $this->author;
-  }
-  
-  public function get($index)
-  {
-    if(isset($this->quotes[$index])){
-      return $this->quotes[$index];
+    
+    protected $author = 'Unknown';
+    
+    public function __construct($quotes = array())
+    {
+        $this->setQuotes($quotes);
     }
-    else{
-      return $this->getRandom();
+    
+    public function setQuotes($quotes)
+    {
+        $this->quotes = $quotes;
     }
-      
-  }
-  
-  public function getRandom()
-  {
-  
-    $max = count($this->quotes);
     
-    $index = floor(rand(0, $max));
+    /**
+    * Set the author of the collection. This enforces that the entire
+    * collection must be "owned" by a single author.
+    * 
+    * @access public
+    * @param mixed $author
+    * @return void
+    */
+    public function setAuthor($author)
+    {
+        $this->author = $author;  
+    }
     
-    return $this->quotes[$index];
+    /**
+    * Returns the author of the quote collection.
+    * 
+    * @access public
+    * @return string
+    */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
     
-  }
+    public function get($index)
+    {
+        if(isset($this->quotes[$index])){
+            return $this->quotes[$index];
+        }
+        else{
+            return $this->getRandom();
+        }
+    
+    }
+
+    /**
+     * getRandom function.
+     * 
+     * @access public
+     * @return string
+     */
+    public function getRandom()
+    {
+    
+        $max = count($this->quotes) - 1;
+        
+        $index = floor(rand(0, $max));
+        
+        return $this->quotes[$index];
+    
+    }
 
 }
